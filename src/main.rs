473e98f8d981936f9d2a452e92663b68b75cabc5
James Sizeland <jamessizeland@gmail.com>
2022-06-13 23:02:50 +0100

    declare tcp listener from tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+// https://github.com/tokio-rs/tokio
+// https://rust-lang.github.io/async-book/
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080")
+        .await
+        .expect("TCP listener failed to start");
+    let (socket, _addr) = listener.accept().await.unwrap();
 }
