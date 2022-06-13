bb1f66c1fda5f1bb6de2632a75183b9f38c84040
James Sizeland <jamessizeland@gmail.com>
2022-06-13 23:42:17 +0100

    implement loop for multiple messages

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-// use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 // https://github.com/tokio-rs/tokio
 // https://rust-lang.github.io/async-book/
 #[tokio::main]
 async fn main() {
+    println!("starting Tokio Server");
     let listener = TcpListener::bind("localhost:8080")
         .await
         .expect("TCP listener failed to start");
-    let (socket, _addr) = listener.accept().await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    let (reader, mut writer) = socket.split();
+
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            println!("Exiting Program");
+            break; // exit the loop gracefully if no data sent
+        }
+
+        writer.write_all(&line.as_bytes()).await.unwrap();
+        line.clear(); // flush the input buffer before the next one
+    }
 }
